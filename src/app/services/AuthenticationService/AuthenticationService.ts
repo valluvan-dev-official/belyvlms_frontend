6ae1dba24ece6d20c468e9db758961714017f148
valluvan-dev-official <btreerepo.pythonfsd@gmail.com>
2026-02-18 11:36:52 +0530

    feat: enable withCredentials for axios API client
    Allow authentication cookies to be sent with cross-origin requests by setting the `withCredentials` flag to true on the axios instance.

--- src/app/services/AuthenticationService/AuthenticationService.ts
+++ src/app/services/AuthenticationService/AuthenticationService.ts
@@ -13,6 +13,7 @@ console.log("🔗 API URL:", BASE_URL);
 
 export const api = axios.create({
   baseURL: BASE_URL,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
